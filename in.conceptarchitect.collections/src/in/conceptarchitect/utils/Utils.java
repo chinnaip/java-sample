4019efcf42521590fdeb6fbd060583124c17a42c
vivekduttamishra <vivek@conceptarchitect.in>
2018-04-16 00:47:28 +0530

    Initial Commit 2

--- in.conceptarchitect.collections/src/in/conceptarchitect/utils/Utils.java
+++ in.conceptarchitect.collections/src/in/conceptarchitect/utils/Utils.java
@@ -1,5 +1,35 @@
 package in.conceptarchitect.utils;
 
-public class Utils {
+import java.util.Random;
 
+public class Utils {
+	public static String toWords(int value) {
+		if(value==0)
+			return "ZERO";
+		
+		String []digits = {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+
+		String sign= value<0 ? "MINUS" : "";
+		
+		String str="";
+		
+		if(value<0) value*=-1;
+		
+		while(value>0) {
+			int d= value%10;
+			str= digits[d]+" "+str;
+			value=value/10;
+		}
+		
+		System.out.println("value created is "+sign+" "+str);
+		
+		return sign+" "+str;
+		
+	}
+
+	public static int randomValue() {
+		Random random=new Random();
+		int value= random.nextInt() % 100;
+		return value;
+	}
 }
