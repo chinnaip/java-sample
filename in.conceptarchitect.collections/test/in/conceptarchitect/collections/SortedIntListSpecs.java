53ace84126196c3981e4f197a1499c6c93953508
vivekduttamishra <vivek@conceptarchitect.in>
2018-04-17 09:42:08 +0530

    spec updated

--- in.conceptarchitect.collections/test/in/conceptarchitect/collections/SortedIntListSpecs.java
+++ in.conceptarchitect.collections/test/in/conceptarchitect/collections/SortedIntListSpecs.java
@@ -1,5 +1,50 @@
 package in.conceptarchitect.collections;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class SortedIntListSpecs {
+	
+	SortedIntList list;
+	
+	@Before
+	public void init() {
+		list=new SortedIntList();
+		list.add(5);
+		list.add(10);
+		list.add(15);
+	}
+
+	@Test
+	public void add_lowestValueIsAddedAtTheBegining() {
+		list.add(1);
+		assertEquals(1, list.get(0));
+		
+	}
+	
+	@Test
+	public void add_highestValueIsAddedAtTheEnd() {
+		list.add(100);
+		assertEquals(100, list.get(-1));
+	}
+	
+	@Test
+	public void add_midValueIsAddedInRightPlace() {
+		list.add(8);
+		assertEquals(8, list.get(1));
+	}
+	
+	
+	@Test
+	@Ignore
+	public void add_allValuesAreSorted() {
+		
+	}
+	
+	
+	
+	
 
 }
